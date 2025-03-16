37472230cb07836bceec536c42d2ea38b899a407
Arian <arian.kulmer@web.de>
2025-03-17 00:03:21 +0530

    sky: Drop bootctl from debug packages
    Change-Id: I4d58ee338929cda9cc53b42253d65f5c3c4807ba

--- device.mk
+++ device.mk
@@ -120,9 +120,6 @@ PRODUCT_PACKAGES += \
     android.hardware.boot-service.qti \
     android.hardware.boot-service.qti.recovery
 
-PRODUCT_PACKAGES_DEBUG += \
-    bootctl
-
 # Camera
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
