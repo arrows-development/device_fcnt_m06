6d8d668ad13991be3b0982a5d866752298da175e
kleidione Freitas <kleidione@gmail.com>
2025-03-19 22:37:43 -0300

    garnet: QtiTelephony: prevent NullPointerException in extractValidBands ([I)
        Error: FATAL EXCEPTION: java.lang.NullPointerException: Attempt to get length of null array in extractValidBands.
    
        A NullPointerException was being thrown in the extractValidBands method when attempting to get the length of a null array. This fix ensures that the array is checked for null before accessing its length, preventing the crash.
    
        Changes:
        - Added null check for the input array (p0) using `if-eqz p0, :return_empty_array`.
        - Introduced a new label `:return_empty_array` to handle the null case.
        - When p0 is null, the method now returns an empty array `new-array v0, v0, [I` instead of crashing.
    
        This ensures that the method gracefully handles null input by returning an empty array, avoiding the NullPointerException.
    
        Stacktrace:
        E AndroidRuntime: FATAL EXCEPTION: main
        E AndroidRuntime: Process: com.qti.phone, PID: 3249
        E AndroidRuntime: java.lang.NullPointerException: Attempt to get length of null array
        E AndroidRuntime:   at com.qti.phone.nruwbicon.NrUwbIconUtils.extractValidBands(NrUwbIconUtils.java:95)
        E AndroidRuntime:   at com.qti.phone.nruwbicon.NrUwbConfigsController.getNrUltraWidebandIconBandsForNsa(NrUwbConfigsController.java:196)
        E AndroidRuntime:   at com.qti.phone.nruwbicon.NrUwbConfigsController.createConfigEntityBySubId(NrUwbConfigsController.java:95)
        E AndroidRuntime:   at com.qti.phone.nruwbicon.NrUwbConfigsController.handleNrUwbConfigsUpdate(NrUwbConfigsController.java:78)
        E AndroidRuntime:   at com.qti.phone.nruwbicon.NrUwbConfigsController.handleMessage(NrUwbConfigsController.java:59)
        E AndroidRuntime:   at android.os.Handler.dispatchMessage(Handler.java:106)
        E AndroidRuntime:   at android.os.Looper.loopOnce(Looper.java:205)
        E AndroidRuntime:   at android.os.Looper.loop(Looper.java:294)
        E AndroidRuntime:   at android.app.ActivityThread.main(ActivityThread.java:8248)
        E AndroidRuntime:   at java.lang.reflect.Method.invoke(Native Method)
        E AndroidRuntime:   at com.android.internal.os.RuntimeInit$MethodAndArgsCaller.run(RuntimeInit.java:552)
        E AndroidRuntime:   at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:971)
    
    Signed-off-by: Kleidione <kleidione@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -67,6 +67,8 @@ lib_fixups: lib_fixups_user_type = {
 
 # Blob fixups
 blob_fixups: blob_fixups_user_type = {
+    'system_ext/priv-app/QtiTelephony/QtiTelephony.apk': blob_fixup()
+        .apktool_patch('patches/'),
     'system_ext/lib64/libwfdmmsrc_system.so': blob_fixup()
         .add_needed('libgui_shim.so'),
     'system_ext/lib64/libwfdnative.so': blob_fixup()
