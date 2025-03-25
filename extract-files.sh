b38838d5478b5c53a196a8499591b3dad729fda1
chandu078 <chandudyavanapelli03@gmail.com>
2025-03-25 18:44:13 -0300

    garnet: Switch to 64 bit only builds
    Change-Id: I0e730663a0e1ad8b18cfa1163b94edf0acd64fff

--- extract-files.sh
+++ extract-files.sh
@@ -65,7 +65,7 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --remove-needed "android.hidl.base@1.0.so" "${2}"
             ;;
-        vendor/bin/hw/android.hardware.security.keymint-service-qti|vendor/lib/libqtikeymint.so|vendor/lib64/libqtikeymint.so)
+        vendor/bin/hw/android.hardware.security.keymint-service-qti|vendor/lib64/libqtikeymint.so)
             [ "$2" = "" ] && return 0
             grep -q "android.hardware.security.rkp-V3-ndk.so" "${2}" || "${PATCHELF_0_17_2}" --add-needed "android.hardware.security.rkp-V3-ndk.so" "${2}"
             ;;
@@ -85,7 +85,7 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             sed -i -E '/media_codecs_(google_audio|google_c2|google_telephony|vendor_audio)/d' "${2}"
             ;;
-        vendor/lib/vendor.libdpmframework.so|vendor/lib64/vendor.libdpmframework.so)
+        vendor/lib64/vendor.libdpmframework.so)
             [ "$2" = "" ] && return 0
             grep -q "libhidlbase_shim.so" "${2}" || "${PATCHELF}" --add-needed "libhidlbase_shim.so" "${2}"
             ;;                      
