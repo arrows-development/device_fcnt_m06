70ad363ad8c6fa70f823cd632e1458996d991284
dianlujitao <dianlujitao@lineageos.org>
2025-03-26 09:58:14 -0300

    garnet: move to aidl bootctrl hal
    Change-Id: I50fd09148097f0cda03c1a105340a3fd5f55ce8d

--- device.mk
+++ device.mk
@@ -134,11 +134,8 @@ PRODUCT_COPY_FILES += \
 
 # Boot control
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-impl-qti \
-    android.hardware.boot@1.2-impl-qti.recovery \
-    android.hardware.boot@1.2-service \
-    bootctrl.garnet \
-    bootctrl.garnet.recovery
+    android.hardware.boot-service.qti \
+    android.hardware.boot-service.qti.recovery
 
 # Camera
 PRODUCT_PACKAGES += \
