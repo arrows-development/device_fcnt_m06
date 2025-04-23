9897b7c4af432341f3f775a009600116a1706001
kailua <kailua@project2by2.jp>
2025-04-24 00:20:17 +0900

    m06: Import sensor stack

--- device.mk
+++ device.mk
@@ -383,6 +383,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.se.omapi.uicc.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_hcesimese/android.hardware.se.omapi.uicc.xml
 
 # Sensors
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@2.1.vendor \
+    libsensorndkbridge
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/sku_parrot/android.hardware.sensor.accelerometer.xml \
     frameworks/native/data/etc/android.hardware.sensor.compass.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/sku_parrot/android.hardware.sensor.compass.xml \
