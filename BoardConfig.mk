41fafd749353c8001e317730b0ec94a309fa4275
kailua <kailua@project2by2.jp>
2025-07-27 02:32:43 +0900

    M06: Nuke libinit

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -82,10 +82,6 @@ $(foreach sku, CN GL JP, \
 
 DEVICE_FRAMEWORK_MANIFEST_FILE += $(DEVICE_PATH)/configs/hidl/framework_manifest.xml
 
-# Init
-TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_M06
-TARGET_RECOVERY_DEVICE_MODULES := libinit_M06
-
 # Kernel
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 BOARD_RAMDISK_USE_LZ4 := true
