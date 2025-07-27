37d2121d557fabc711fda73eb997b507e4983f16
kailua <kailua@project2by2.jp>
2025-07-27 10:28:09 +0900

    M06: Update to V06RM67D

--- M06.mk
+++ M06.mk
@@ -34,6 +34,6 @@ CUSTOM_PROCESSOR_INFO := Qualcomm Snapdragon 7s Gen 2
 PRODUCT_GMS_CLIENTID_BASE := android-fcnt
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="M06-user 14 V01RM44C M06.20240614 release-keys" \
-    BuildFingerprint=FCNT/M06/M06:14/V01RM44C/M06.20240614:user/release-keys \
+    BuildDesc="M06-user 14 V06RM67D M06.20250425 release-keys" \
+    BuildFingerprint=FCNT/M06/M06:14/V06RM67D/M06.20250425:user/release-keys \
     DeviceProduct=M06
