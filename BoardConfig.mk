a24b8e621e798eaaccb8ab021cf6b62e64600b6b
kailua <kailua@project2by2.jp>
2025-08-04 23:18:39 +0900

    M06: Don't set {swinfo,mtdoops}.fingerprint
    * Not actually needed

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -87,9 +87,7 @@ BOARD_KERNEL_BASE := 0x00000000
 
 BOARD_KERNEL_CMDLINE := \
     video=vfb:640x400,bpp=32,memsize=3072000 \
-    disable_dma32=on \
-    swinfo.fingerprint=$(CUSTOM_VERSION) \
-    mtdoops.fingerprint=$(CUSTOM_VERSION)
+    disable_dma32=on
 
 BOARD_BOOTCONFIG := \
     androidboot.hardware=qcom \
